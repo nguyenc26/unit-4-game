a7ffb5b21d133c224fdd8e102379657614880853
Cuong Nguyen <nguyenc72694@gmail.com>
2019-07-18 17:28:24 -0500

    added some pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+//Psuedocode 
+//=====================
+    //at game start 
+        //random numnber is displayed between 19 -120
+        //crystals are assigned a random hidden value between 1-12 
+
+    //Listens for a crystal click 
+        //add the points to the total score 
+    //keeps listening to clicks 
+        //add points until they match the total score(random # generated above)
+
+    //if they match their score with the random display number
+        //they win (alerts the win in the box)
+            //game resets 
+                //tracks the wins 
+                //New random number 
+                //new hidden values for the crystals 
+    //if they go over their score with the random display number 
+        //they lose (alerts the lost in the box)
+            //game resets 
+                //tracks the losses 
+                //new random number 
+                //new hidden values for the crystals
