1aed30929f74566376a505b258978c55351ee2d8
Cuong Desktop <nguyenc72694@gmail.com>
2019-07-21 17:12:04 -0500

    added display information to the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,48 @@
 //Psuedocode 
 //=====================
-    //at game start 
-        //random numnber is displayed between 19 -120
-        //crystals are assigned a random hidden value between 1-12 
+//at game start 
+//random numnber is displayed between 19 -120
+//crystals are assigned a random hidden value between 1-12 
 
-    //Listens for a crystal click 
-        //add the points to the total score 
-    //keeps listening to clicks 
-        //add points until they match the total score or go over(random # generated above)
+//Listens for a crystal click 
+//add the points to the total score 
+//keeps listening to clicks 
+//add points until they match the total score or go over(random # generated above)
 
-    //if they match their score with the random display number
-        //they win (alerts the win in the box)
-            //game resets 
-                //tracks the wins 
-                //New random number 
-                //new hidden values for the crystals 
-    //if they go over their score with the random display number 
-        //they lose (alerts the lost in the box)
-            //game resets 
-                //tracks the losses 
-                //new random number 
-                //new hidden values for the crystals
+//if they match their score with the random display number
+//they win (alerts the win in the box)
+//game resets 
+//tracks the wins 
+//New random number 
+//new hidden values for the crystals 
+//if they go over their score with the random display number 
+//they lose (alerts the lost in the box)
+//game resets 
+//tracks the losses 
+//new random number 
+//new hidden values for the crystals
 //======================
 
 //Variables 
 //=============================
-var randomNum = [];
+var randomNum = Math.floor(Math.random() * 101 + 19);
+console.log(randomNum);
 var wins = 0;
 var losses = 0;
-var red
-var green
-var blue
-var purple
-var yourScore
+var red = [];
+var green = [];
+var blue = [];
+var purple = [];
+var yourScore = [];
 //============================
 
+//Main Game
+//============================
+$(document).ready(function () {
 
-$(document).ready(function() {
-
+$("#randomizedNum").html("Random Number <br>" + randomNum);
+$("#yourScore").html("Your Current Score <br>" + yourScore);
+//Dont forget to add the annoucement of when you win and lose in here later. 
+$(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br>");
 
 });
