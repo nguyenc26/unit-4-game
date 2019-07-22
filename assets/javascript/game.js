bfb089a5a891876564f8783f499ba9163eca8f6c
Cuong Desktop <nguyenc72694@gmail.com>
2019-07-21 21:03:52 -0500

    final save for the night

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,7 @@
 //=============================
 var wins = 0;
 var losses = 0;
-var yourScore = [];
+var yourScore = 0;
 //============================
 
 //Main Game
@@ -27,48 +27,47 @@ var yourScore = [];
 $(document).ready(function () {
 
     //function startGame() {
-        //at game start 
-        //random numnber is displayed between 19 -120
-        var randomNum = Math.floor(Math.random() * 101 + 19);
-        $("#randomizedNum").html("Random Number <br>" + randomNum);
-        console.log(randomNum);
+    //at game start 
+    //random numnber is displayed between 19 -120
+    var randomNum = Math.floor(Math.random() * 101 + 19);
+    $("#randomizedNum").html("Random Number <br>" + randomNum);
+    console.log(randomNum);
 
-        //generates a random # and puts them into an array
-        var crystalarr = []
-        //like a 4 loop??
-        while (crystalarr.length < 4) {
-            var i = Math.floor(Math.random() * 11) + 1;
-            if (crystalarr.indexOf(i) === -1) crystalarr.push(i);
-        }
-        console.log(crystalarr)
-        //on a click it grabs the number generated above and associates it with the crystal
-        $("#red").on("click", function() {
-            console.log(crystalarr[0]);
-        });
-        $("#green").on("click", function() {
-            console.log(crystalarr[1]);
-        });
-        $("#blue").on("click", function() {
-            console.log(crystalarr[2]);
-        });
-        $("#purple").on("click", function() {
-            console.log(crystalarr[3]);
-        });
+    //generates a random # and puts them into an array
+    var crystalarr = []
+    //like a 4 loop??
+    while (crystalarr.length < 4) {
+        var i = Math.floor(Math.random() * 11) + 1;
+        if (crystalarr.indexOf(i) === -1) crystalarr.push(i);
+    }
+    console.log(crystalarr)
+    
+    //on a click it grabs the number generated above and associates it with the crystal
+    $("#red").on("click", function () {
+        console.log(crystalarr[0] + yourScore);
+    });
+    $("#green").on("click", function () {
+        console.log(crystalarr[1]);
+    });
+    $("#blue").on("click", function () {
+        console.log(crystalarr[2]);
+    });
+    $("#purple").on("click", function () {
+        console.log(crystalarr[3]);
+    });
 
-        
 
-        //Display wins and losses 
-        $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br>");
 
-    //}
+    //Display wins and losses 
+    $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br>");
+    $("#yourScore").html("Your Score <br>" + yourScore);
+
 
     //===========================================================
     //Listens for a crystal click 
     //add the points to the total score 
     //keeps listening to clicks 
     //add points until they match the total score or go over(random # generated above)
-
-
 
     // if (yourScore === randomNum) {
     //     wins++;
