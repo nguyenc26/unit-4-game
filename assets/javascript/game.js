8733e4beb37b3f093084fcd8000282456b6d6276
Cuong XPS <nguyenc72694@gmail.com>
2019-07-23 18:37:32 -0500

    game functions and scores display but the values are adding wrong when the game resets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,84 +26,93 @@ var yourScore = 0;
 //============================
 $(document).ready(function () {
 
-    //function startGame() {
-    //at game start 
-    //random numnber is displayed between 19 -120
-    var randomNum = Math.floor(Math.random() * 101 + 19);
-    $("#randomizedNum").html("Random Number <br>" + randomNum);
-    console.log(randomNum);
-    $("#yourScore").html("Your Score <br>" + yourScore);
-    $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br>");
+    function startGame() {
+        yourScore = 0;
 
-    //generates a random # and puts them into an array
-    var crystalarr = []
-    //like a 4 loop??
-    while (crystalarr.length < 4) {
-        var i = Math.floor(Math.random() * 11) + 1;
-        if (crystalarr.indexOf(i) === -1) crystalarr.push(i);
+        //at game start 
+        //random numnber is displayed between 19 -120
+        var randomNum = Math.floor(Math.random() * 101 + 19);
+        $("#randomizedNum").html("Random Number <br>" + randomNum);
+        console.log(randomNum);
+        $("#yourScore").html("Your Score <br>" + yourScore);
+        $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br>");
+
+        //generates a random # and puts them into an array
+        var crystalarr = []
+        //like a 4 loop??
+        while (crystalarr.length < 4) {
+            var i = Math.floor(Math.random() * 11) + 1;
+            if (crystalarr.indexOf(i) === -1) crystalarr.push(i);
+        }
+        console.log(crystalarr)
+
+        //when scores are added and resets they are adding the old values. It has something to do with the stuff from above. Changing the crystalarr stuff. 
+
+        //on a click it grabs the number generated above and associates it with the crystal
+        $("#red").on("click", function () {
+            console.log("BAM")
+            var num = parseInt(crystalarr[0]);
+            yourScore += num;
+            $("#yourScore").html("Your Score <br>" + yourScore);
+            if (yourScore > randomNum) {
+                losses++;
+                startGame();
+                $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You lose!");
+            } else if (yourScore === randomNum) {
+                wins++;
+                startGame();
+                $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You win!");
+
+            }
+            console.log(yourScore);
+        });
+        $("#green").on("click", function () {
+            var num = parseInt(crystalarr[1]);
+            yourScore += num;
+            $("#yourScore").html("Your Score <br>" + yourScore);
+            if (yourScore > randomNum) {
+                losses++;
+                startGame();
+                $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You lose!");                
+            } else if (yourScore === randomNum) {
+                wins++;
+                startGame();
+                $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You win!");
+            }
+            console.log(yourScore);
+        });
+        $("#blue").on("click", function () {
+            var num = parseInt(crystalarr[2]);
+            yourScore += num;
+            $("#yourScore").html("Your Score <br>" + yourScore);
+            if (yourScore > randomNum) {
+                losses++;
+                startGame();
+                $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You lose!");
+            } else if (yourScore === randomNum) {
+                wins++;
+                startGame();
+                $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You win!");               
+            }
+            console.log(yourScore);
+        });
+        $("#purple").on("click", function () {
+            var num = parseInt(crystalarr[3]);
+            yourScore += num;
+            $("#yourScore").html("Your Score <br>" + yourScore);
+            if (yourScore > randomNum) {
+                losses++;
+                startGame();
+                $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You lose!");
+            } else if (yourScore === randomNum) {
+                wins++;
+                startGame();
+                $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You win!");
+            }
+            console.log(yourScore);
+        });
     }
-    console.log(crystalarr)
-
-    //on a click it grabs the number generated above and associates it with the crystal
-    $("#red").on("click", function () {
-        var num = parseInt(crystalarr[0]);
-        yourScore += num;
-        $("#yourScore").html("Your Score <br>" + yourScore);
-        if (yourScore > randomNum) {
-            losses++;
-            $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You lose!" );
-        } else if (yourScore === randomNum) {
-            wins++;
-            $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You win!" );
-        }
-        console.log(yourScore);
-    });
-    $("#green").on("click", function () {
-        var num = parseInt(crystalarr[1]);
-        yourScore += num;
-        $("#yourScore").html("Your Score <br>" + yourScore);
-        if (yourScore > randomNum) {
-            losses++;
-            $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You lose!" );
-            console.log('you lose');
-        } else if (yourScore === randomNum) {
-            wins++;
-            $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You win!" );
-            console.log('you win');
-        }
-        console.log(yourScore);
-    });
-    $("#blue").on("click", function () {
-        var num = parseInt(crystalarr[2]);
-        yourScore += num;
-        $("#yourScore").html("Your Score <br>" + yourScore);
-        if (yourScore > randomNum) {
-            losses++;
-            $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You lose!" );
-            console.log('you lose');
-        } else if (yourScore === randomNum) {
-            wins++;
-            $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You win!" );
-            console.log('you win');
-        }
-        console.log(yourScore);
-    });
-    $("#purple").on("click", function () {
-        var num = parseInt(crystalarr[3]);
-        yourScore += num;
-        $("#yourScore").html("Your Score <br>" + yourScore);
-        if (yourScore > randomNum) {
-            losses++;
-            $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You lose!" );
-            console.log('you lose');
-        } else if (yourScore === randomNum) {
-            wins++;
-            $(".tracker").html("Wins= " + wins + "<br><br><br>" + "Losses= " + losses + "<br><br><br> You win!" );
-            console.log('you win');
-        }
-        console.log(yourScore);
-    });
-
+    startGame();
 
 
     //Display wins and losses 
